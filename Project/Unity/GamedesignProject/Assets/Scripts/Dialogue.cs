6d254565e5060c25f373a3c37a3750f5e5243064
saluji <erwin_oudomvylay@hotmail.de>
2023-12-20 17:00:12 +0100

    Dialogue system testing

--- Project/Unity/GamedesignProject/Assets/Scripts/Dialogue.cs
+++ Project/Unity/GamedesignProject/Assets/Scripts/Dialogue.cs
@@ -6,23 +6,19 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 
-public class Textbox : MonoBehaviour
+public class Dialogue : MonoBehaviour
 {
     public GameObject player;
+    public ScriptMachine scriptMachine;
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
-    private ScriptMachine playerMovement;
-    private Animator animator;
     public string[] dialogue;
     private int index = 0;
     public float wordSpeed;
-    public bool playerIsClose;
-    int x;
 
-    void Awake()
+    private void Awake()
     {
-        playerMovement = player.GetComponent<ScriptMachine>();
-        animator = player.GetComponent<Animator>();
+
     }
     //Start dialogueText string at 1
     void Start()
@@ -33,15 +29,11 @@ public class Textbox : MonoBehaviour
     void Update()
     {
         //Checks if player is in range and hits interact button
-        if ((Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Z)) && playerIsClose)
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Z))
         {
             if (!dialoguePanel.activeInHierarchy)
             {
                 dialoguePanel.SetActive(true);
-                //Disable player movement while in dialogue
-                playerMovement.enabled = false;
-                //Player stays idle when activating dialogue while walking
-                animator.SetFloat("speed", 0);
                 StartCoroutine(Typing());
             }
             //Advandes next line of text
@@ -63,7 +55,6 @@ public class Textbox : MonoBehaviour
         dialogueText.text = "";
         index = 0;
         dialoguePanel.SetActive(false);
-        playerMovement.enabled = true;
     }
     //Creates written text
     IEnumerator Typing()
@@ -85,23 +76,6 @@ public class Textbox : MonoBehaviour
         }
         else
         {
-            RemoveText();
-        }
-    }
-    //Activates trigger if Player is in NPC range
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = true;
-        }
-    }
-    //Deactivates trigger if Player is not in NPC range anymore
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerIsClose = false;
             RemoveText();
         }
     }
