d22d7b5f64f4fe0dc7422850d08c301ebc45b1f7
saluji <erwin_oudomvylay@hotmail.de>
2023-12-21 05:56:39 +0100

    Code cleaning
    dle animation when talking while walking
    changed encapsule variables

---
+++ Project/Unity/GamedesignProject/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+//using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.EventSystems;
+using Unity.VisualScripting;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private string[] dialogue;
+    [SerializeField] private float wordSpeed;
+    [SerializeField] private int index = 0;
+    private ScriptMachine playerSM;
+    private RaycastHit2D hit;
+    //public string nameText;
+
+    void Awake()
+    {
+        //playerSM = player.GetComponent<ScriptMachine>();
+        //raycast = player.GetComponent<RaycastHit2D>();
+    }
+    //Start dialogueText string at 1
+    void Start()
+    {
+        
+        dialogueText.text = "";
+    }
+    //Dialogue check
+    void Update()
+    {
+        //Checks if player is in range and hits interact button
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!dialoguePanel.activeInHierarchy)
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+            //Advandes next line of text
+            else if (dialogueText.text == dialogue[index])
+            {
+                NextLine();
+            }
+        //Displays all text at once without strings overflowing
+        }
+        if (Input.GetKeyDown(KeyCode.X) && dialoguePanel.activeInHierarchy)
+        {
+            StopAllCoroutines();
+            dialogueText.text = dialogue[index];
+        }
+    }
+    //Removes text, reactivates player movement
+    void RemoveText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+    //Creates written text
+    IEnumerator Typing()
+    {
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+    //Advances to next line of text
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            RemoveText();
+        }
+    }
+}
